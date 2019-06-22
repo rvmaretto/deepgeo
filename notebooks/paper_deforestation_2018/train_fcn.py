65efdcee7a67b91c106c50a55fe3cfe069a3c1e4
rvmaretto <rvmaretto@gmail.com>
2019-06-21 21:05:04 -0400

    Changes for #53

--- notebooks/paper_deforestation_2018/train_fcn.py
+++ notebooks/paper_deforestation_2018/train_fcn.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 
 sys.path.insert(0, '../../src')
 import deepgeo.networks.model_builder as mb
+import deepgeo.networks.loss_funcions as lossf
 
 # # Load input Dataset
 
@@ -25,45 +26,11 @@ model_dir = os.path.join(DATA_DIR, 'tf_logs', 'experiments', network,
                          'test_%s_%s' % (network, datetime.now().strftime('%Y_%m_%d-%H_%M_%S')))
 
 
-def parse(serialized):
-    features = {'label': tf.FixedLenFeature([], tf.string, default_value=''),
-                'height': tf.FixedLenFeature([], tf.int64, default_value=0),
-                'width': tf.FixedLenFeature([], tf.int64, default_value=0)}
-
-    parsed_features = tf.parse_single_example(serialized=serialized, features=features)
-    height = parsed_features['height']
-    width = parsed_features['width']
-
-    label = tf.decode_raw(parsed_features['label'], tf.int32)
-    label = tf.reshape(label, [height, width, 1])
-
-    return label
-
-
-def compute_weights_mean_proportion(tfrecord, classes, classes_zero=['no_data']):
-    tf.enable_eager_execution()
-    train_ds = tf.data.TFRecordDataset(tfrecord)
-    train_ds = train_ds.map(parse)
-    tot_count = [0] * len(classes)
-    for label in train_ds:
-        label = label.numpy()
-        unique, count = np.unique(label, return_counts=True)
-        for k, v in enumerate(unique):
-            if classes[v] not in classes_zero:
-                tot_count[v] += count[k]
-    total = sum(tot_count)
-    proportions = [i / total for i in tot_count]
-    mean_prop = sum(proportions) / (len(proportions) - len(classes_zero))
-    weights = [mean_prop / i if i != 0 else 0 for i in proportions]
-    return weights
-
-
-weights_train = compute_weights_mean_proportion(train_tfrecord, class_names, ['no_data'])
-weights_eval = compute_weights_mean_proportion(test_tfrecord, class_names, ['no_data'])
+weights_train = lossf.compute_weights_mean_proportion(train_tfrecord, class_names, ['no_data'])
+weights_eval = lossf.compute_weights_mean_proportion(test_tfrecord, class_names, ['no_data'])
 
 
 # Train the Network
-
 params = {
     'epochs': 100,
     'batch_size': 20,
@@ -78,7 +45,7 @@ params = {
     'chips_tensorboard': 2,
     # 'dropout_rate': 0.5,  # TODO: Put a bool parameter to apply or not Dropout
     'fusion': 'early',
-    'loss_func': 'weighted_crossentropy',
+    'loss_func': 'weighted_cross_entropy',
     'data_aug_ops': ['rot90', 'rot180', 'rot270', 'flip_left_right',
                      'flip_up_down', 'flip_transpose'],
     'data_aug_per_chip': 4,
