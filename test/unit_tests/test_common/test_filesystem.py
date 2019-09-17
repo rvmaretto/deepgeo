050ab620c642afd4b48c60b6cba2674b8452b183
rvmaretto <rvmaretto@gmail.com>
2019-09-17 19:23:28 -0400

    Fixing issues on tests.

--- test/unit_tests/test_common/test_filesystem.py
+++ test/unit_tests/test_common/test_filesystem.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'src'))
-import common.filesystem as fs
+import deepgeo.common.filesystem as fs
 
 
 def test_directoryCreationAndDeletion():
